72acd6b94cedb03962dbe05c01e5c99edc45f3d8
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-21 19:45:01 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -76,7 +76,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   movie = Movie.create(title: "Wat?")
-  Movie.update(movie.id, title: "Wat, huh?")
+  movie.update(title: "Wat, huh?")
   movie.save
 end
 
